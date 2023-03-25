7974921fac7ea4073193327c1cbf6f2b3135e32f
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-03-25 23:36:48 +0600

    initial commit

---
+++ src/components/Country/Country.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Country.css";
+
+const Country = (props) => {
+    const { flags, name, area, region, population } = props.country;
+    // console.log(props.country);
+    return (
+        <div className="country">
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt={flags.alt} />
+            <p>Area: {area}</p>
+            <p>Population: {population}</p>
+            <p><small>Region: {region}</small></p>
+        </div>
+    );
+};
+
+export default Country;
